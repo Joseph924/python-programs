6d8024da8781953bfd2b6c76c96772ba30a083ae
Joseph924 <75637939+Joseph924@users.noreply.github.com>
2022-02-09 08:55:38 +0300

    Add files via upload

---
+++ ifstatement.py
@@ -0,0 +1,9 @@
+# Program checks if the number is positive or negative
+# And displays an appropriate message
+
+num = 3
+
+if num >= 0:
+    print("Positive or Zero")
+else:
+    print("Negative number")
---
+++ variables2.py
@@ -0,0 +1,4 @@
+x = 5
+y = "John"
+print(type(x)) #getting the data type of a variable 
+print(type(y))
